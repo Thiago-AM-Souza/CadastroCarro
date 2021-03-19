2bb639285e1aaffdd13d2ab101bac07a48fd86cb
Thiago-AM-Souza <80932188+Thiago-AM-Souza@users.noreply.github.com>
2021-03-18 23:28:38 -0300

    update
    adicionei scanner

--- src/Teste.java
+++ src/Teste.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+
 public class Teste {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
         int numVeiculos;
+        input.close();
+
     }
 }
