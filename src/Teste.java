f9a3651a3339e0e16db4ceaff0842f834348c4fa
João Kitajima <joao.kitajima@outlook.com>
2021-03-18 23:29:30 -0300

    modificao teste

--- src/Teste.java
+++ src/Teste.java
@@ -3,8 +3,20 @@ import java.util.Scanner;
 public class Teste {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        
         int numVeiculos;
-        input.close();
 
+        System.out.println("Quantos veículos você quer cadastrar?");
+        numVeiculos = input.nextInt();
+
+        Carro[] carrinhos = new Carro[numVeiculos];
+
+        // for 1
+
+        System.out.println("\n\n");
+
+        // for 2
+
+        input.close();
     }
 }
